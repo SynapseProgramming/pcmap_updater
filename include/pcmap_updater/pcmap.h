a98ca8c7451919c05b0a9db765beed33bac6f174
Roald Ong <roald.ong@gmail.com>
2024-01-31 18:56:58 +0800

    Add pcl_conversions and pcl_ros dependencies. Published processed pointcloud

--- include/pcmap_updater/pcmap.h
+++ include/pcmap_updater/pcmap.h
@@ -1,6 +1,10 @@
 #ifndef PCMAP_H
 #define PCMAP_H
 
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
 #include <ros/ros.h>
 
 #include <pcmap_updater/pcl_utils.hpp>
@@ -13,11 +17,21 @@ class PCMAP {
  private:
   std::vector<Eigen::Vector3d> map_points;
   ProbabilisticMap probMap;
+  ros::NodeHandle nh_;
+  ros::Publisher pc_pub_;
+
+  pcl::PointCloud<pcl::PointXYZ> pc;
+  sensor_msgs::PointCloud2 output_msg;
+
+  bool ready;
 
  public:
-  PCMAP() : probMap(0.1) {
+  PCMAP() : nh_("~"), probMap(0.1), ready(false) {
     ReadPointsFromPCD("/home/ro/Documents/pcd_files/office_val_183.pcd",
                       map_points);
+
+    std::cout << "map points size: " << map_points.size() << "\n";
+    pc_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("voxeled", 10, true);
 
     // insert points into prob map
     for (Vector3D iter : map_points) {
@@ -25,6 +39,24 @@ class PCMAP {
       probMap.addInitPoint(iter);
     }
     // load points back into a point cloud
+    std::vector<CoordT> points;
+    probMap.getOccupiedVoxels(points);
+    std::cout << "size: " << points.size() << "\n";
+
+    for (CoordT pts : points) {
+      pcl::PointXYZ pclpt(pts.x, pts.y, pts.z);
+      // pclpt._x = pts.x;
+      // pclpt._y = pts.y;
+      // pclpt._z = pts.z;
+      pc.push_back(pclpt);
+    }
+    pcl::toROSMsg(pc, output_msg);
+    output_msg.header.frame_id = "map";
+    ready = true;
+  }
+  void publish_new() {
+    if (ready == false) return;
+    pc_pub_.publish(output_msg);
   }
 };
 
--- src/pcmap.cpp
+++ src/pcmap.cpp
@@ -1,13 +1,11 @@
-#include <ros/ros.h>
-
 #include <pcmap_updater/pcmap.h>
-
+#include <ros/ros.h>
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "pcmap");
   PCMAP map;
+    map.publish_new();
+    ros::spin();
 
-
-  ros::spin();
   return 0;
 }
